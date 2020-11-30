a648beeee7c1b75cd7891fa2ab697b3d7035c5a3
Rao Fahad <raofahad046@gmail.com>
2020-11-30 14:30:14 +0500

    added css to app.css file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import React from 'react';
 import './App.css';
-
+import { Header } from './components/Header';
 function App() {
   return (
     <div>
-      Hello World
+      <Header/>
     </div>
   );
 }
