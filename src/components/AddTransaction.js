6fd6c567d7b7562ca19fd3cbb82f97ce424ff15f
Rao Fahad <raofahad046@gmail.com>
2020-11-30 15:41:25 +0500

    added useState() in AddTransaction component

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,19 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 const AddTransaction = () => {
+    const [description, setDescription] = useState();
+    const [transactionAmount, settransactionAmount] = useState();
     return (
         <div>
             <h3>Add New Transaction</h3>
             <form>
                 <div className="form-control">
                     <label htmlfor="description">Description</label>
-                    <input type="text" id="description" placeholder="Detail of Transaction" />
+                    <input type="text" 
+                            id="description" 
+                            placeholder="Detail of Transaction" 
+                            value={description} 
+                            onChange={(e) => setDescription(e.target.value) }
+                    />
                 </div>
                 <div className="form-control">
                     <label htmlFor="transactionamount">
                         Transaction Amount
                     </label>
-                    <input type="number" id="transactionamount" placeholder="Enter transaction amount" />
+                    <input type="number"
+                            id="transactionamount" 
+                            placeholder="Enter transaction amount" 
+                            value={transactionAmount} 
+                            onChange={(e) => settransactionAmount(e.target.value)
+                    />
                     <button className="btn">Add Transaction</button>
                 </div>
             </form>
